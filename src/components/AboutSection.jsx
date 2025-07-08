effc8c713be4002f5cbeb2ab5fb0360f2e05d204
Anmol Jain <anmoljain79421@gmail.com>
2025-07-08 23:48:31 +0530

    first commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1j1uEp0hBNHT2YIinO3wLPkzgpV5pq7m_/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1IDvQs-1wV8fUvtOJM9dqLR0OOUYO7pVR/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
