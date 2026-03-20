8012caa514b80ece14fd4472ec60ee2ddc788f09
Anmol Jain <anmoljain79421@gmail.com>
2026-03-20 16:05:00 +0530

    merge conflict resolved

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -29,7 +29,6 @@ export const EducationSection = () => {
             </div>
             <span className="text-sm md:text-base text-muted-foreground font-medium">
               2025-2026
-              Present 46462d8999873ae5d30c2b4420a100184db25287
             </span>
           </div>
 
