dc07a92a40bd0e98724a9523a67cae474d22d1e4
Anmol Jain <anmoljain79421@gmail.com>
2026-04-16 11:18:20 +0530

    Update About & Experience

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -15,7 +15,7 @@ export const ExperienceSection = () => {
         <div className="space-y-12">
           {[{
             role: "Associate AI Engineer",
-            company: "NavAjna Technologies, Hyderabad(T.S)",
+            company: "NavAjna Technologies Pvt Ltd, Hyderabad(T.S)",
             logo: navAjnaLogo,
             year: "MAR 2026 - PRESENT",
             description: 'Working as a Associate AI Engineer, focusing on developing AI/ML-based solutions and backend applications using Python..',
@@ -23,7 +23,7 @@ export const ExperienceSection = () => {
           },
           {
             role: "Development Intern",
-            company: "IMPACTQA SERVICES PVT LTD, Noida(U.P)",
+            company: "ImpactQA Services Pvt Ltd, Noida(U.P)",
             logo: impactqaLogo,
             year: "FEB 2025 - MAY 2025",
             certificateLink:
@@ -33,7 +33,7 @@ export const ExperienceSection = () => {
           },
           {
             role: "Summer Intern",
-            company: "CIENA INDIA PVT LTD, Gurgaon(Haryana)",
+            company: "Ciena India Pvt Ltd, Gurgaon(H.R)",
             logo: cienaLogo,
             year: "JUNE 2024 - SEPT 2024",
             certificateLink:
